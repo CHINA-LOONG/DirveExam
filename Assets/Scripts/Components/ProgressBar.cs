0949ae8834e093e2c5ca2a220b089d771f05c5a1
china_loong <xuelong9009@qq.com>
2018-07-15 20:00:08 +0800

    细化UI界面

---
+++ Assets/Scripts/Components/ProgressBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ProgressBar : MonoBehaviour
+{
+    public Image imgProgress;
+
+    [Range(0.0f, 1.0f)]
+    private float _value;
+
+    public float Value
+    {
+        get { return imgProgress.fillAmount; }
+        set { imgProgress.fillAmount = value; }
+    }
+}
--- Assets/Scripts/UIScripts/UIMainWindow.cs
+++ Assets/Scripts/UIScripts/UIMainWindow.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMainWindow : UIWindow
 {
-    
+    public Text textProg;
+    public ProgressBar ProgressBar;
+
+
     protected override void BindListener()
     {
         base.BindListener();
